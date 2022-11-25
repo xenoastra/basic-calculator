9490dd69310f5c5a543e4fa066faa7fa1cfc8960
Vanessa L <70068643+xenoastra@users.noreply.github.com>
2022-11-24 22:52:27 -0500

    added while loop and menu

--- basic calculator.c
+++ basic calculator.c
@@ -1,28 +1,64 @@
-int main()
-{
-    double num1;
-    double num2;
-    char oper;
+#include <stdio.h>
+#include <string.h>
 
-    printf("Enter a number: ");
-    scanf("%lf", &num1);
-    printf("Enter an operator: ");
-    scanf(" %c", &oper);
-    printf("Enter another number: ");
-    scanf("%lf", &num2);
+void calc(){
 
-   
-   if(oper == '+'){
-       printf("%f", num1 + num2);
-   } else if(oper == '-'){
-       printf("%f", num1 - num2);
-   } else if(oper == '*'){
-       printf("%f", num1 * num2);
-   } else if(oper == '/'){
-       printf("%f", num1 / num2);
-   } else{
-       printf("Invalid operator");
-   }
+int input;
+while (input <= 5){
+  printf("\n------------------------------------------------------");
+  printf("\nWelcome to the calculator program!");
+  printf("\nPlease choose the operation you desire by entering the appropriate number:\n");
+  printf("\n1. addition\n2. subtraction\n3. multiplication\n4. division\n5. exit\n");
+  printf("------------------------------------------------------");
+  printf("\n\nEnter a number: ");
+  int input;
+  double num1;
+  double num2;
+  scanf("%d", &input);
+  
+    if(input == 1){
+        printf("Enter the first number: ");
+         scanf("%lf", &num1);
+         printf("Enter another number: ");
+         scanf("%lf", &num2);
+         printf("%f", num1 + num2);
+      }
+    else if(input == 2){
+         printf("Enter the first number: ");
+         scanf("%lf", &num1);
+         printf("Enter another number: ");
+         scanf("%lf", &num2);
+         printf("%f", num1 - num2);
+     }
+    else if(input == 3){
+         printf("Enter the first number: ");
+         scanf("%lf", &num1);
+         printf("Enter another number: ");
+         scanf("%lf", &num2);
+         printf("%f", num1 * num2);
+     }
+    else if(input == 4){
+         printf("Enter the first number: ");
+         scanf("%lf", &num1);
+         printf("Enter another number: ");
+         scanf("%lf", &num2);
+         printf("%f", num1 / num2);
+     }
+    else if (input == 5){
+      printf("done\n");
+      exit(1);
+     }
+    else{
+      printf("invalid, enter a number from 1 to 5");
+    }
+}
 
+}
+
+int main(){
+  calc();
     return 0;
 }
+
+
+
